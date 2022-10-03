3d98dfcc91cb44d5e152c6d473a2634cc9034238
Simon Fink <simon.2.fink@gmail.com>
2022-10-03 12:27:02 +0200

    Switch between normal and responsive layout in navigation

--- header-navigation-responsive/app.js
+++ header-navigation-responsive/app.js
@@ -1,2 +1,24 @@
 const menu = document.querySelector('nav.navbar .menu');
 menu.addEventListener('click', () => { alert('Show responsive menu')});
+
+
+const mediaQueryList = window.matchMedia("(max-width: 600px)");
+mediaQueryList.addEventListener('change', setNavItemsVisibility)
+
+function setNavItemsVisibility(e) {
+    e.matches ? hideNavItems() : showNavItems();
+}
+
+function hideNavItems() {
+    const navItems = document.querySelectorAll('nav.navbar > .navitem');
+    navItems.forEach((item) => {
+        item.style.display = 'none';
+    });
+}
+  
+function showNavItems() {
+    const navItems = document.querySelectorAll('nav.navbar > .navitem');
+    navItems.forEach((item) => {
+        item.style.display = 'unset';
+    });
+}
