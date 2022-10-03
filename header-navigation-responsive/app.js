4c344a801a8a690b9d5f2bb1c2227d061868e319
Simon Fink <simon.2.fink@gmail.com>
2022-10-03 12:40:10 +0200

    Toggle visibility of responsive menu

--- header-navigation-responsive/app.js
+++ header-navigation-responsive/app.js
@@ -1,5 +1,7 @@
+let navItemsVisible;
+
 const menu = document.querySelector('nav.navbar .menu');
-menu.addEventListener('click', () => { alert('Show responsive menu')});
+menu.addEventListener('click', toggleNavItemsVisibility);
 
 
 const mediaQueryList = window.matchMedia("(max-width: 600px)");
@@ -9,11 +11,16 @@ function setNavItemsVisibility(e) {
     e.matches ? hideNavItems() : showNavItems();
 }
 
+function toggleNavItemsVisibility() {
+    navItemsVisible ? hideNavItems() : showNavItems();
+}
+
 function hideNavItems() {
     const navItems = document.querySelectorAll('nav.navbar > .navitem');
     navItems.forEach((item) => {
         item.style.display = 'none';
     });
+    navItemsVisible = false;
 }
   
 function showNavItems() {
@@ -21,4 +28,5 @@ function showNavItems() {
     navItems.forEach((item) => {
         item.style.display = 'unset';
     });
+    navItemsVisible = true;
 }
